478ff6b2eaa3755578afe11e5d6cd65d4e558111
Scott Branston <scott.branston@gmail.com>
2020-12-23 16:41:01 -0800

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -2,21 +2,28 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './components/directorycomponent';
 import './App.css';
+import { CAMPSITES } from './shared/campsites';
 
 class App extends Component {
-  render() {
-      return (
-          <div className="App">
-              <Navbar dark color="primary">
-              <div className="container">
-                  <NavbarBrand href="/">NuCamp</NavbarBrand>
-              </div>
-              </Navbar>
-              <Directory />
-              <button onClick={() => alert('Button Clicked!')}>Click me</button>
-          </div>
-      );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES
+        };
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Navbar dark color="primary">
+                <div className="container">
+                    <NavbarBrand href="/">NuCamp</NavbarBrand>
+                </div>
+                </Navbar>
+                <Directory campsites = {this.state.campsites} />
+            </div>
+        );
+    }
 }
 // the function App is the default export for this entire file
 export default App;
