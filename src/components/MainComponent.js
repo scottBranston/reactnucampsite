7e090c55245e1d7cbd604ac1f6f0c44475f9aefa
Scott Branston <scott.branston@gmail.com>
2020-12-30 18:07:59 -0800

    React Router part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,29 +3,33 @@ import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES,
-            selectedCampsite: null
+            campsites: CAMPSITES
         };
     }
 
-    onCampsiteSelect(campsiteId) {
-        this.setState({selectedCampsite: campsiteId});
-        // NEVER do this to change a state:  this.state.selectedCampsite = campsite;
-        // The constructor is the only place you can assign a value to state properties directly
-    }
-
     render() {
+        const HomePage = () => {
+            return (
+                <Home />
+            );
+        };
+
         return (
             <div>
                 <Header />
-                <Directory campsites = {this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)} />
-                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Redirect to='/home' />
+                </Switch>
                 <Footer />
             </div>
         );
