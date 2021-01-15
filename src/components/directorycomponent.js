8ef1ae2dc83da5c075c58ef76b4acabe89c455a0
Scott Branston <scott.branston@gmail.com>
2021-01-15 14:32:01 -0800

    Fetch Handling Errors

--- src/components/directorycomponent.js
+++ src/components/directorycomponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
-import { Promotions } from '../redux/promotions';
+//import { Promotions } from '../redux/promotions';
 import { baseUrl } from '../shared/baseUrl';
 
 function RenderDirectoryItem({campsite}) {
